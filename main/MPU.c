b8fa811aec5b9f2672acdc29d5b0123e2b6b89b0
Saswata Halder <haldersaswata558@gmail.com>
2025-05-12 15:17:06 +0530

    Reading Accelerometer and Gyro data
    MPU.c now reads Accelerometer and Gyroscope data from the dedicated registers (6- axis).
    The raw values are then converted to g units (for Acc) and deg / s (for Gyro).

--- main/MPU.c
+++ main/MPU.c
@@ -1,61 +1,55 @@
 #include <stdio.h>
-#include "esp_log.h"
 #include "driver/i2c_master.h"
+#include "esp_log.h"
 #include "freertos/FreeRTOS.h"
 
-#define port I2C_NUM_0
-#define SDA 21
 #define SCL 22
-#define scl_speed 400000
+#define SDA 21
+#define scl_speed 100000
 #define MPU_ADDR 0x68
 #define MPU_PWR 0x6B
+#define port I2C_NUM_0
 
-#define GYRO_CON 0x1B
-#define ACC_CON 0x1C
+// #define Accel_con 0x1C
+// #define Gyro_con 0x1B
 
 i2c_master_bus_handle_t bus_handle;
 i2c_master_dev_handle_t dev_handle;
 
-#define Acc_Out_H 0x3B
-
-static const char *TAG = "MPU";
-
 void i2c_init()
 {
-    // configure the master bus
-    i2c_master_bus_config_t bus_conf = {
-        .i2c_port = port,
-        .sda_io_num = SDA,
+    i2c_master_bus_config_t bus_con = {
         .scl_io_num = SCL,
+        .sda_io_num = SDA,
+        .i2c_port = port,
         .clk_source = I2C_CLK_SRC_DEFAULT,
         .glitch_ignore_cnt = 7,
         .flags.enable_internal_pullup = true,
     };
-    // allocate and initialize the master bus
-    ESP_ERROR_CHECK(i2c_new_master_bus(&bus_conf, &bus_handle));
+
+    ESP_ERROR_CHECK(i2c_new_master_bus(&bus_con, &bus_handle));
 }
 
 void mpu_init()
 {
-    i2c_device_config_t dev_conf = {
+    i2c_device_config_t dev_con = {
         .dev_addr_length = I2C_ADDR_BIT_7,
         .device_address = MPU_ADDR,
         .scl_speed_hz = scl_speed,
     };
-    ESP_ERROR_CHECK(i2c_master_bus_add_device(bus_handle, &dev_conf, &dev_handle));
+
+    ESP_ERROR_CHECK(i2c_master_bus_add_device(bus_handle, &dev_con, &dev_handle));
 }
 
-void wake_mpu()
+void mpu_wake()
 {
-    uint8_t data_buffer[2] = {MPU_PWR, 0x00};
-    uint8_t acc_scale[2] = {ACC_CON, 0x00};
-    uint8_t gyro_scale[2] = {GYRO_CON, 0x00};
+    uint8_t data[2] = {MPU_PWR, 0x00};
+    // uint8_t acc_con[2] = {Accel_con, 0x00};
+    // uint8_t gyro_scale[2] = {Gyro_con, 0x00};
 
-    // array of buffer info
-    i2c_master_transmit_multi_buffer_info_t buffer_info[3] = {
-        {.write_buffer = data_buffer, .buffer_size = sizeof(data_buffer)},
-        {.write_buffer = acc_scale, .buffer_size = sizeof(acc_scale)},
-        {.write_buffer = gyro_scale, .buffer_size = sizeof(gyro_scale)},
+    i2c_master_transmit_multi_buffer_info_t buffer_info[1] = {
+        {.write_buffer = data, .buffer_size = sizeof(data)},
+        // {.write_buffer = gyro_scale, .buffer_size = sizeof(gyro_scale)},
     };
 
     ESP_ERROR_CHECK(i2c_master_multi_buffer_transmit(dev_handle, buffer_info, sizeof(buffer_info) / sizeof(i2c_master_transmit_multi_buffer_info_t), -1));
@@ -63,19 +57,24 @@ void wake_mpu()
 
 void read_accel()
 {
-    uint8_t reg = 0x3B; // ACCEL_XOUT_H — first of 14 sequential registers
-    uint8_t data[6] = {0};
+    uint8_t reg = 0x3B;
+    uint8_t data[14] = {0};
 
     while (1)
     {
-        esp_err_t err = i2c_master_transmit_receive(dev_handle, &reg, 1, data, 6, -1);
-        // Decode and print accel values
+        ESP_ERROR_CHECK(i2c_master_transmit_receive(dev_handle, &reg, 1, data, 14, -1));
+
         int16_t ax = (data[0] << 8) | data[1];
         int16_t ay = (data[2] << 8) | data[3];
         int16_t az = (data[4] << 8) | data[5];
+        int16_t gx = (data[8] << 8) | data[9];
+        int16_t gy = (data[10] << 8) | data[11];
+        int16_t gz = (data[12] << 8) | data[13];
 
         float aRes = 2.0 / 32768.0;
-        printf("Accel Gs: X=%.3f | Y=%.3f | Z=%.3f\n", ax * aRes, ay * aRes, az * aRes);
+        float gRes = 250.0 / 32768.0;
+
+        printf("Acc X = %.3f | Acc Y = %.3f | Acc Z = %.3f | Gyro X = %.3f | Gyro Y = %.3f | Gyro Z = %.3f\n", ax * aRes, ay * aRes, az * aRes, gx * gRes, gy * gRes, gz * gRes);
 
         vTaskDelay(pdMS_TO_TICKS(1000));
     }
@@ -83,16 +82,16 @@ void read_accel()
 
 void app_main(void)
 {
-    ESP_LOGI(TAG, "Initializing I2C Master....");
+    printf("Initializing I2C Master....\n");
     i2c_init();
 
-    ESP_LOGI(TAG, "Initializing I2C Slave(MPU)....");
+    printf("Initializing I2C Slave....\n");
     mpu_init();
 
     vTaskDelay(pdMS_TO_TICKS(100));
 
-    ESP_LOGI(TAG, "Waking up I2C Slave(MPU)....");
-    wake_mpu();
+    printf("Waking up MPU....\n");
+    mpu_wake();
 
     vTaskDelay(pdMS_TO_TICKS(100));
 
